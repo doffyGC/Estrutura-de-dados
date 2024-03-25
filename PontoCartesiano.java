cea8816277a3d1f01230d9070c6d1f8d5dfecaca
Guilherme Chagas <128249294+doffyGC@users.noreply.github.com>
2024-03-24 23:18:02 -0300

    Create PontoCartesiano.java
    Classe ponto cartesiano feito para a atividade 1 do marco 1

---
+++ PontoCartesiano.java
@@ -0,0 +1,32 @@
+public class PontoCartesiano {
+
+   private double pontoX;
+    private double pontoY;
+
+    public PontoCartesiano(double pontoX, double pontoY){
+
+        this.pontoX = pontoX;
+        this.pontoY = pontoY;
+    }
+
+
+    public double getPontoX() {
+        return pontoX;
+    }
+
+    public double getPontoY() {
+        return pontoY;
+    }
+
+    public void setPontoX(double pontoX) {
+        this.pontoX = pontoX;
+    }
+
+    public void setPontoY(double pontoY){this.pontoY = pontoY;}
+
+    public double calcularDistancia(PontoCartesiano outroPonto){
+
+        double distancia = Math.sqrt(Math.pow((outroPonto.pontoX - this.pontoX), 2) + Math.pow((outroPonto.pontoY- this.pontoY), 2));
+        return distancia;
+    }
+}
